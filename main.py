0f030d35538e83f1c165d52cfa0c45c0f84c02f2
Vuittonlim <vuittonkk01@gmail.com>
2026-03-07 22:37:55 +0800

    Updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+import sys
+sys.path.append('/csc2106-project')
 import json
 from mqtt_client import get_client, publish
 from config import MQTT_TOPIC
@@ -8,6 +10,12 @@ def read_sensors():
     pass
 
 def main():
+    # Take note there's a limit of 100 sessions, each run constitutes as one session so use this wisely. 
     client = get_client()
+    try:
+        payload = json.dumps({"message": "hello from Pico W"})
+        publish(client, MQTT_TOPIC, payload)
+    except Exception as e:
+        print("Publish failed:", e)
    
 main()
